e40ef3be347a0c2b58a0b20778a2385972bbb10f
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-06-20 16:49:08 +0200

    datepicker updated and loading function

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ async function loadBitCoin() {
     responseAsJson = await response.json();
     bit.push(responseAsJson);
     bitToday(responseAsJson);
-
+    setPlaceholder();
     console.log(responseAsJson);
 }
 
@@ -126,17 +126,27 @@ function bitTable() {
 }
 
 async function updateDate() {
+    loading();
     startDate = document.getElementById('startData').value;
     endDate = document.getElementById('endData').value;
     await loadBitCoin();
     chart();
     chartBar();
     bitTable();
+    loadingComplete();
     document.getElementById('canvas').classList.remove('d-none');
     //   document.getElementById('exchange').classList.remove('d-none');
     document.getElementById('canvas').scrollIntoView({
         behavior: 'smooth'
     });
+}
+
+function loading() {
+    document.getElementById('progress').classList.remove('d-none');
+}
+
+function loadingComplete() {
+    document.getElementById('progress').classList.add('d-none');
 }
 
 function showChartOne() {
@@ -310,82 +320,26 @@ function scrollToHistory() {
     });
 }
 
+/*---------------------------set-datepicker---------------------------*/
 
+$(function() {
+    var dtToday = new Date();
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate() - 1;
+    var year = dtToday.getFullYear();
+    if (month < 10)
+        month = '0' + month.toString();
+    if (day < 10)
+        day = '0' + day.toString();
 
+    var minDate = year + '-' + month + '-' + day;
 
+    $('#endData').attr('max', minDate);
+    $('#startData').attr('max', minDate);
 
-
-
-
-
-
-
-
-/*
-anychart.onDocumentReady(function() {
-
-    // create a data set
-    let data = anychart.data.set([
-
-        ["January", 16000, 8000],
-        ["February", 15000, 12000],
-        ["March", 16000, 18000],
-        ["April", 15000, 11000],
-        ["May", 14000, 9000]
-
-
-
-    ]);
-
-    // map the data
-    var seriesData_1 = data.mapAs({ x: 0, value: 1 });
-    var seriesData_2 = data.mapAs({ x: 0, value: 2 });
-
-    // create a chart
-    var chart = anychart.area();
-
-    // set the interactivity mode
-    chart.interactivity().hoverMode("single");
-
-    // create the first series, set the data and name
-    var series1 = chart.area(seriesData_1);
-    series1.name("2004");
-
-    // configure the visual settings of the first series
-    series1.normal().fill("#00cc99", 0.3);
-    series1.hovered().fill("#00cc99", 0.1);
-    series1.selected().fill("#00cc99", 0.5);
-    series1.normal().stroke("#00cc99", 1, "10 5", "round");
-    series1.hovered().stroke("#00cc99", 2, "10 5", "round");
-    series1.selected().stroke("#00cc99", 4, "10 5", "round");
-
-    // create the second series, set the data and name  
-    var series2 = chart.area(seriesData_2);
-    series2.name("2005");
-
-    // configure the visual settings of the second series
-    series2.normal().fill("#0066cc", 0.3);
-    series2.hovered().fill("#0066cc", 0.1);
-    series2.selected().fill("#0066cc", 0.5);
-    series2.normal().hatchFill("forward-diagonal", "#0066cc", 1, 15);
-    series2.hovered().hatchFill("forward-diagonal", "#0066cc", 1, 15);
-    series2.selected().hatchFill("forward-diagonal", "#0066cc", 1, 15);
-    series2.normal().stroke("#0066cc");
-    series2.hovered().stroke("#0066cc", 2);
-    series2.selected().stroke("#0066cc", 4);
-
-    // set the chart title
-    chart.title("Area Chart: Appearance");
-
-    // set the titles of the axes
-    chart.xAxis().title("Timeline");
-    chart.yAxis().title("Value, $");
-
-    // set the container id
-    chart.container("container");
-
-    // initiate drawing the chart
-    chart.draw();
 });
 
-*/
+function setPlaceholder() {
+    document.getElementById("startData").placeholder = "start";
+    document.getElementById("endData").placeholder = "end";
+}
